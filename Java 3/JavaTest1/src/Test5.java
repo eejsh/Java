38762f044f1ba42c92211051b91ac5fdaa517e83
eejang <105977239+eejsh@users.noreply.github.com>
2022-06-10 17:22:11 +0900

    Update Test5.java

--- Java 3/JavaTest1/src/Test5.java
+++ Java 3/JavaTest1/src/Test5.java
@@ -3,18 +3,32 @@ public class Test5 {
 
 	public static void main(String[] args) {
 		
-		
-		
 		int a = 1;
 		int total = 0;
 		 
 		do {
 			total = total + a; 	
 			a++;
-		}while(a<=100 && total<=4000);
+		}while(a<=100 && total<4000);
 	
 		System.out.println(total);
 		
-	}
+//	}
+//
+//}
 
-}
+    int w = 1;
+    int sum2 = 0;
+    while(w<=100) {
+        if(sum2 < 4000 && w<89) {
+    		sum2 += w;
+    	}
+    	else {
+    		break;
+    	}
+    	w++;
+          }
+      System.out.println(sum2);
+    
+      }
+    }
